4664887bb6262fb357a62d562332e644989c96b2
kishan pokar <kishanipokar@gmail.com>
2026-04-21 12:04:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+const port = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const debugRoutes = require("./debug");
+app.use(debugRoutes);
+
+const commentsRoutes = require("./comments");
+app.use(commentsRoutes);
+
+const convertRoutes = require("./convert");
+app.use(convertRoutes);
+
+app.use(express.static(path.join(__dirname)));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
